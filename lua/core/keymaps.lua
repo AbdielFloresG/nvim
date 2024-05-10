f47aca26471f3325b275c2bc43d1ccffe362bf71
AbdielFloresG <abdielfg@gmail.com>
2024-05-10 00:21:39 -0700

    Cambio a Lua

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,32 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.scrolloff = 8
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<leader>y", '"+y')
+
+-- GIT
+
+-- vim.keymap.set("n", "<C-p>", ":GFiles<CR>")
+vim.keymap.set("n", "<leader>pf", ":GFiles<CR>")
+vim.keymap.set("n", "<C-j>", ":cnext")
+vim.keymap.set("n", "<C-k>", ":cprev")
---
+++ lua/core/plugin_config/completions.lua
@@ -0,0 +1,20 @@
+local cmp = require("cmp")
+
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-o>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  }),
+})
