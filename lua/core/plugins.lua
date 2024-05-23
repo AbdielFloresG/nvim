2d2a7ad2d3c2be12f03693ce9f29f179a1ced348
AbdielFloresG <abdielfg@gmail.com>
2024-05-23 14:01:14 -0700

    none ls

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,43 +1,46 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  "nvim-tree/nvim-tree.lua",
-  "nvim-tree/nvim-web-devicons",
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-lualine/lualine.nvim",
-  "tpope/vim-fugitive",
-  -- completion
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip",
-  "rafamadriz/friendly-snippets",
-  -- "github/copilot.vim",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  -- {
-    -- "vinnymeller/swagger-preview.nvim",
-    -- run = "npm install -g swagger-ui-watcher",
-  -- },
-  -- {
-    -- "iamcco/markdown-preview.nvim",
-    -- run = "cd app && npm install",
-  -- },
-  {
-    "nvim-telescope/telescope.nvim", tag = "0.1.4",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	"nvim-tree/nvim-tree.lua",
+	"nvim-tree/nvim-web-devicons",
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-lualine/lualine.nvim",
+	"tpope/vim-fugitive",
+	-- completion
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+	-- "github/copilot.vim",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"nvimtools/none-ls.nvim",
+
+	-- {
+	-- "vinnymeller/swagger-preview.nvim",
+	-- run = "npm install -g swagger-ui-watcher",
+	-- },
+	-- {
+	-- "iamcco/markdown-preview.nvim",
+	-- run = "cd app && npm install",
+	-- },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 })
