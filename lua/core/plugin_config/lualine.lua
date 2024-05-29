cb2100a4aafacc812f2e1ffec31c277af53081f1
Abdiel Flores <abdielfg@gmail.com>
2024-05-28 20:04:32 -0700

    Snippets and copilot working

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,14 +1,14 @@
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'nightfly',
-  },
-  sections = {
-    lualine_a = {
-      {
-        'filename',
-        path = 1,
-      }
-    }
-  }
-}
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "catppuccin",
+	},
+	sections = {
+		lualine_a = {
+			{
+				"filename",
+				path = 1,
+			},
+		},
+	},
+})
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -21,10 +21,14 @@ require("lazy").setup({
 	-- completion
 	"hrsh7th/nvim-cmp",
 	"hrsh7th/cmp-nvim-lsp",
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
-	"rafamadriz/friendly-snippets",
-	-- "github/copilot.vim",
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	"github/copilot.vim",
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
