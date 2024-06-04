b6a57a57c7b7caac634383d62650ca2d73b55faf
AbdielFloresG <abdielfg@gmail.com>
2024-06-04 12:49:16 -0700

    Back to nvim-tree

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
+    "hrsh7th/cmp-nvim-lsp",
   },
   {
     "L3MON4D3/LuaSnip",
@@ -14,6 +14,13 @@ return {
     config = function()
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- local has_words_before = function()
+      -- 	unpack = unpack or table.unpack
+      -- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      -- 	return col ~= 0
+      -- 		and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      -- end
 
       cmp.setup({
         snippet = {
@@ -31,6 +38,15 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- ["<Leader>c"] = cmp.mapping(function(fallback)
+          -- 	if cmp.visible() then
+          -- 		cmp.select_next_item()
+          -- 	elseif has_words_before() then
+          -- 		cmp.complete()
+          -- 	else
+          -- 		fallback()
+          -- 	end
+          -- end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
