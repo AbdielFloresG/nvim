310afe5a49b41e7b0500797716ca1869d5295c26
Abdiel Flores <abdielfg@gmail.com>
2024-07-23 19:15:27 -0700

    Indent blankline

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,14 +1,22 @@
 return {
-	{
-		"brenoprata10/nvim-highlight-colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
-	},
-	{
-		"github/copilot.vim",
-	},
-	{
-		"tpope/vim-commentary",
-	},
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup({})
+    end,
+  },
+  {
+    "github/copilot.vim",
+  },
+  {
+    "tpope/vim-commentary",
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+    config = function()
+      require("ibl").setup()
+    end,
+  },
 }
