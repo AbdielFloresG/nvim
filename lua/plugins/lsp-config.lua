87145a07b79ec33dac0c4fd055fabdda64889d6d
AbdielFloresG <abdielfg@gmail.com>
2024-08-05 11:57:18 -0700

    Go support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,8 +2,17 @@ return {
 	{
 		"williamboman/mason.nvim",
 		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 		config = function()
-			require("mason").setup()
+			require("mason").setup({
+				ensure_installed = {
+					"prettierd",
+					"eslint_d",
+					"stylua",
+				},
+			})
 		end,
 	},
 	{
@@ -14,7 +23,13 @@ return {
 		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
+				ensure_installed = {
+					"tailwindcss",
+					"tsserver",
+					"html",
+					"lua_ls",
+					"gopls",
+				},
 			})
 		end,
 	},
@@ -37,6 +52,20 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
@@ -45,5 +74,22 @@ return {
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
 		end,
+	},
+	{
+		"olexsmir/gopher.nvim",
+		ft = "go",
+		-- branch = "develop", -- if you want develop branch
+		-- keep in mind, it might break everything
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
+		},
+		-- (optional) will update plugin's deps on every update
+		build = function()
+			vim.cmd.GoInstallDeps()
+		end,
+		---@type gopher.Config
+		opts = {},
 	},
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,11 +5,16 @@ return {
 	},
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 		null_ls.setup({
 			sources = {
 				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.golines,
 				-- null_ls.builtins.formatting.rubocop,
 			},
 			on_attach = function(client, bufnr)
