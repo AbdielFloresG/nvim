b958eaca453c6594db762bbd59ea120f2f8970d4
AbdielFloresG <abdielfg@gmail.com>
2024-11-14 23:51:00 -0700

    php support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,112 +1,132 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason").setup({
-				ensure_installed = {
-					"prettier",
-					"eslint_d",
-					"stylua",
-					"golines",
-					"gofumpt",
-					"goimports",
-					"goimports-reviser",
-					"golines",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"tailwindcss",
-					"ts_ls",
-					"html",
-					"lua_ls",
-					"gopls",
-					"astro",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require("mason").setup({
+        ensure_installed = {
+          "prettier",
+          "eslint_d",
+          "stylua",
+          "golines",
+          "gofumpt",
+          "goimports",
+          "goimports-reviser",
+          "golines",
+          "phpactor",
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "tailwindcss",
+          "ts_ls",
+          "html",
+          "lua_ls",
+          "gopls",
+          "astro",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				cmd = { "gopls" },
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-				settings = {
-					gopls = {
-						completeUnimported = true,
-						usePlaceholders = true,
-						analyses = {
-							unusedparams = true,
-						},
-					},
-				},
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "svelte",
+          "vue",
+          "astro",
+        },
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
+      })
 
-			lspconfig.astro.setup({
-				capabilities = capabilities,
-				cmd = { "astro-ls", "--stdio" },
-				filetypes = { "astro" },
-				root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
-				init_options = {
-					typescript = {},
-				},
-			})
+      lspconfig.astro.setup({
+        capabilities = capabilities,
+        cmd = { "astro-ls", "--stdio" },
+        filetypes = { "astro" },
+        root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
+        init_options = {
+          typescript = {},
+        },
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-		end,
-	},
-	{
-		"olexsmir/gopher.nvim",
-		ft = "go",
-		-- branch = "develop", -- if you want develop branch
-		-- keep in mind, it might break everything
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
-		},
-		-- (optional) will update plugin's deps on every update
-		build = function()
-			vim.cmd.GoInstallDeps()
-		end,
-		---@type gopher.Config
-		opts = {},
-	},
+      lspconfig.phpactor.setup({
+        capabilities = capabilities,
+        cmd = { "phpactor", "language-server" },
+        filetypes = { "php" },
+        root_dir = lspconfig.util.root_pattern("composer.json"),
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    -- branch = "develop", -- if you want develop branch
+    -- keep in mind, it might break everything
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
+    },
+    -- (optional) will update plugin's deps on every update
+    build = function()
+      vim.cmd.GoInstallDeps()
+    end,
+    ---@type gopher.Config
+    opts = {},
+  },
 }
